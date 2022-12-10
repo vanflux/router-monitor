14aa6d9c2ea058d82fd4c86caa31d05076307459
vanflux <lucasabruzzi@hotmail.com>
2022-12-10 01:02:21 -0300

    Simple sidebar

--- web/src/components/sidebar/sidebar.tsx
+++ web/src/components/sidebar/sidebar.tsx
@@ -1,9 +1,9 @@
-import { TreeView } from "@mui/lab";
 import TreeItem, { TreeItemProps } from "@mui/lab/TreeItem";
-import { Divider, Grid, MenuItem, Select, Typography } from "@mui/material";
+import { Button, Divider, Grid, Typography } from "@mui/material";
 import { ReactComponent as Logo } from '../../assets/logo.svg';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import PodcastsIcon from '@mui/icons-material/Podcasts';
+import PhoneIphoneIcon from '@mui/icons-material/PhoneIphone';
+import BlockIcon from '@mui/icons-material/Block';
 
 export function SidebarItem({ children, label, ...props }: TreeItemProps) {
   return <TreeItem {...props} label={
@@ -16,40 +16,29 @@ export function SidebarItem({ children, label, ...props }: TreeItemProps) {
 }
 
 export function Sidebar() {
-  return <Grid item padding={2} sx={{ color: 'primary.contrastText', bgcolor: 'primary.main' }}>
-    <Grid my={2} container item>
+  return <Grid width={350} padding={2} sx={{ color: 'primary.contrastText', bgcolor: 'primary.main' }}>
+    <Grid my={3} container item>
       <Grid container justifyContent='center'>
         <Typography noWrap variant='h5'>Router Monitor</Typography>
       </Grid>
-      <Grid container justifyContent='center'>
+      <Grid mt={2} container justifyContent='center'>
         <Logo width={64} height={64} fill='white' />
       </Grid>
     </Grid>
     <Divider />
-    <Grid my={2} container item>
-      <Grid container justifyContent='center'>
-        <Typography mb={1} variant='subtitle1'>Agents:</Typography>
-      </Grid>
-      <Select sx={{ bgcolor: 'white' }} fullWidth>
-        <MenuItem value='1'>Agent 1</MenuItem>
-        <MenuItem value='2'>Agent 2</MenuItem>
-        <MenuItem value='3'>Agent 3</MenuItem>
-      </Select>
-    </Grid>
-    <Divider />
-    <Grid my={2} container item>
-      <TreeView
-        defaultCollapseIcon={<ExpandMoreIcon />}
-        defaultExpandIcon={<ChevronRightIcon />}
-      >
-        <SidebarItem nodeId="1" label="Info" />
-        <SidebarItem nodeId="1" label="Wifi Clients">
-          <SidebarItem nodeId="2" label="RSSI Report" />
-        </SidebarItem>
-        <SidebarItem nodeId="1" label="Restriction">
-          <SidebarItem nodeId="2" label="Mac Blocking" />
-        </SidebarItem>
-      </TreeView>
+    <Grid my={3} container item>
+      {[
+        [<PodcastsIcon />, 'Agents'],
+        [<PhoneIphoneIcon />, 'Wifi Clients'],
+        [<BlockIcon />, 'Restriction'],
+      ].map(([icon, text]) =>
+        <Button fullWidth color='inherit' variant='text'>
+          <Grid padding={0.5} container item>
+            {icon}
+            <Typography ml={1} color='contrastText'>{text}</Typography>
+          </Grid>
+        </Button>
+      )}
     </Grid>
   </Grid>
 }
